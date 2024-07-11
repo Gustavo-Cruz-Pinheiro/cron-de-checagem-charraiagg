3d3b5a32992bd7baebc35ca3ddd016bb6feb8f22
Gustavo-Cruz-Pinheiro <gustavo.pinheiro@estudante.ifms.edu.br>
2024-07-11 09:50:21 -0400

    fix: funcionamento do cron

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import axios from 'axios';
 
-cron.schedule('*/5 * * * *', async () => {
+cron.schedule('*/4 * * * *', async () => {
   try {
     await axios.get('https://charraiagg-back-end.onrender.com/health');
     console.log('Keep-alive request successful');
