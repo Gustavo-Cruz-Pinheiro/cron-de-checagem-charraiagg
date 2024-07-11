76858ac2d04072e658c7eec812560788bff4d843
Gustavo-Cruz-Pinheiro <gustavo.pinheiro@estudante.ifms.edu.br>
2024-07-11 12:49:26 -0400

    update: change request update method

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-import cron from 'node-cron';
-import axios from 'axios';
+// import cron from 'node-cron';
+// import axios from 'axios';
 
-cron.schedule('*/4 * * * *', async () => {
-  try {
-    await axios.get('https://charraiagg-back-end.onrender.com/health');
-    console.log('Keep-alive request successful');
-  } catch (error) {
-    console.error('Error keeping API alive:', error);
-  }
-});
+// cron.schedule('*/4 * * * *', async () => {
+//   try {
+//     await axios.get('https://charraiagg-back-end.onrender.com/health');
+//     console.log('Keep-alive request successful');
+//   } catch (error) {
+//     console.error('Error keeping API alive:', error);
+//   }
+// });
+
+import { get } from 'https';
+
+const url = 'https://charraiagg-back-end.onrender.com/health'; // Substitua pelo URL da sua API
+
+setInterval(() => {
+  get(url, (res) => {
+    console.log(`Status Code: ${res.statusCode}`);
+  }).on('error', (e) => {
+    console.error(`Erro: ${e.message}`);
+  });
+}, 300000); // 300000ms = 5 minutos
